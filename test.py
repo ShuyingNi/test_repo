d32b992f178ee2a0d3d4e9b5999f5e0beb4c90c1
ShuyingNi <shuying_ni@alumni.brown.edu>
2019-07-14 12:13:22 +0800

    testcommit

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ import shutil
 import sys
 import struct
 import tempfile
-
+import pandas as pd
 # Useful for very coarse version differentiation.
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
